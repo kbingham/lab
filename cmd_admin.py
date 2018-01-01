cdac18eb15d19185c8d11d609b105d7cc79a0c40
Kieran Bingham <kieran@bingham.xyz>
2018-01-01 13:41:46 +0000

    admin: Provide simple upgrade mechanism

--- cmd_admin.py
+++ cmd_admin.py
@@ -1,4 +1,5 @@
 import cmd
+import os
 
 
 class AdminCommands(cmd.Cmd):
@@ -7,6 +8,7 @@ class AdminCommands(cmd.Cmd):
 
     def do_upgrade(self, line):
         print("Upgrading installation")
+        os.system("git fetch && git reset --hard origin/master")
 
     def default(self, line):
         return False
