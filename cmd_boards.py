f5aca448dce1098c34d0d896920794b079dd1f48
Kieran Bingham <kieran@bingham.xyz>
2021-02-18 20:12:13 +0000

    boards: Add hooks to run board commands
    Actually runs the commands
    
    Signed-off-by: Kieran Bingham <kieran@bingham.xyz>

--- cmd_boards.py
+++ cmd_boards.py
@@ -1,18 +1,25 @@
 import cmd
+import os
 
+power = "energenie-power"
+serial = "serial"
 
 class BoardCommands(cmd.Cmd):
     def do_on(self, board):
         print("switching on", board)
+        os.system(power + " " + board + " on")
 
     def do_off(self, board):
         print("switching off", board)
+        os.system(power + " " + board + " off")
 
     def do_reboot(self, board):
         print("rebooting ", board)
+        os.system(power + " " + board + " reboot")
 
     def do_serial(self, board):
         print("Connecting to Serial...")
+        os.system(serial + " " + board)
 
     def default(self, line):
         return False
